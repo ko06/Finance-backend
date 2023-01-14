0cc45a187e3390130982661cf39b7ede0b551342
selva-tuva <selva@tuvalabs.com>
2023-01-14 21:24:56 +0530

    added react app to django

--- finserve/urls.py
+++ finserve/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import home
 
 urlpatterns = [
-    path('', include('members.urls')),
+    path("", home, name="home"),
+    # path('', include('members.urls')),
     path('misc/', include('misc.urls')),
     path('admin/', admin.site.urls),
 ]
