4836dc470465dd8f6e769c27ba4a1530ec17446d
vimalraj9423 <vimalraj9423@gmail.com>
2023-03-07 23:50:11 +0530

    config

--- finserve/settings.py
+++ finserve/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-(!5ngh2ctsx^2$*$7%y4p&5*3e3c%n)@xt%7&6jr%+!53kj0ko
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
